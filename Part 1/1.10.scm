a071f23c64e27d1efe23aed650687cbc6a4a0291
Arseny Bulaev <seni190999@mail.ru>
2023-01-28 18:23:56 +0300

    scary Ackermann function

---
+++ Part 1/1.10.scm
@@ -0,0 +1,21 @@
+#lang scheme
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+           (A x (- y 1))))))
+
+
+;; (A 0 n) -> 2*n
+(display "(A 0 n): \n")
+(A 0 10)
+(A 0 9)
+(A 0 8)
+
+;; (A 1 n) -> 2^n
+(display "(A 1 n): \n")
+(A 1 10)
+(A 1 9)
+(A 1 8)
